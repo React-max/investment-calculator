dde55c3a5147e971de70affd176ea227bf59d46f
Md. Abid Raihan <abidcsedu@gmail.com>
2023-12-04 16:16:44 +0600

    calculate and show table data

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,4 +1,11 @@
-const ResultsTable = () => {
+const formatter = new Intl.NumberFormat("en-US", {
+	style: "currency",
+	currency: "USD",
+	minimumFractionDigits: 2,
+	maximumFractionDigits: 2,
+});
+
+const ResultsTable = (props) => {
 	return (
 		<div>
 			{/* Todo: Show below table conditionally (only once result data is available) */}
@@ -14,13 +21,26 @@ const ResultsTable = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>YEAR NUMBER</td>
-						<td>TOTAL SAVINGS END OF YEAR</td>
-						<td>INTEREST GAINED IN YEAR</td>
-						<td>TOTAL INTEREST GAINED</td>
-						<td>TOTAL INVESTED CAPITAL</td>
-					</tr>
+					{props.data.map((yearData) => (
+						<tr key={yearData.year}>
+							<td>{yearData.year}</td>
+							<td>{formatter.format(yearData.savingsEndOfYear)}</td>
+							<td>{formatter.format(yearData.yearlyInterest)}</td>
+							<td>
+								{formatter.format(
+									yearData.savingsEndOfYear -
+										props.initialInvestment -
+										yearData.yearlyContribution * yearData.year
+								)}
+							</td>
+							<td>
+								{formatter.format(
+									props.initialInvestment +
+										yearData.yearlyContribution * yearData.year
+								)}
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</div>
